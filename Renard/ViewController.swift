c4e629e607df7c2fbb5a3dc4b3e2faf07f044977
Cristian Rojas <cristian.rojas@live.fr>
2020-06-10 18:38:32 +0200

    replaced uitableviewcontroller, now we use a normal viewcontroller with protocols

--- Renard/ViewController.swift
+++ Renard/ViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-class ViewController: UITableViewController {
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
     var activities = [Activity]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+    
         var activity = Activity(name: "Meditación", goal: 500, count: 1)
         activities.append(activity)
         activity = Activity(name: "Natación", goal: 500, count: 1)
@@ -20,27 +22,30 @@ class ViewController: UITableViewController {
         activity = Activity(name: "Programación", goal: 500, count: 1)
         activities.append(activity)
         activity = Activity(name: "Bicicleta", goal: 500, count: 1)
+        activities.append(activity)
         
-    }
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.activities.count
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let activity = activities[indexPath.row]
-        let cellID = "ActivityCell"
-    
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath) as! ActivityCell
-        cell.nameLabel.text = activity.name
-        cell.countLabel.text = "\(activity.count!)"
-        cell.goalLabel.text = "\(activity.goal!)"
-      
-        return cell
     }
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+          return 1
+      }
+      func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+          return self.activities.count
+      }
+      
+      func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+          let activity = activities[indexPath.row]
+          let cellID = "ActivityCell"
+      
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath) as! ActivityCell
+          cell.nameLabel.text = activity.name
+          cell.countLabel.text = "\(activity.count!)"
+          cell.goalLabel.text = "\(activity.goal!)"
+        
+          return cell
+      }
+
+}
